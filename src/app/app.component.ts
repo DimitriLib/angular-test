15ca8d93a7f6f47a3e1ef5069880ba9bd2392c99
dimitri.liberman <dimitri.liberman@cgi.com>
2023-06-12 10:13:51 -0700

    added tinymce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'my-app';
+  ngOnInit() {
+    console.log('test');
+    console.log('test');
+    console.log('test0');
+    console.log('test0');
+    console.log('test0');
+    console.log('test0');
+    console.log('test0');
+    console.log('test0');
+    console.log('test0');
+
+  }
+  ngOnDestroy() {
+
+  }
 }
